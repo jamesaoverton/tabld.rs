4d7c63e67b6f084906581ca472282e89910cde4d
Sebastian Duesing <66700705+sebastianduesing@users.noreply.github.com>
2026-07-23 18:11:56 -0700

    Start on subset extraction

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
-use clap::Parser;
+use clap::{Args, Parser, Subcommand};
 use regex::{Error, Regex};
 use std::collections::HashSet;
 use std::fs;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
-use tabld::model::{ANNOTATED_SOURCE, ANNOTATED_TARGET, AXIOM, Object};
 use tabld::{
-    model::{ANNOTATION_PROPERTY, Graph, IndexedMemoryGraph, MemoryGraph, SUBCLASS_OF, Subject},
+    mireot::mireot_extract,
+    model::{Graph, IndexedMemoryGraph},
     rdfxml,
 };
 
 #[derive(Parser, Debug)]
-#[command(name = "rustbot", version, about, long_about = None)]
-struct Args {
+#[command(name = "extract", version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Extracts a tree of terms from inputs
+    Mireot(MireotArgs),
+    Subset(SubsetArgs),
+}
+
+#[derive(Args, Debug)]
+struct MireotArgs {
     ///load ontology from a file
     #[arg(short, long, value_name = "file")]
     input: String,
@@ -44,6 +57,29 @@ struct Args {
     ///path to file of root terms of branches to extract
     #[arg(short = 'B', long = "branch-from-terms", value_name = "textfile")]
     branch_from_terms: Option<Vec<String>>,
+
+    ///set the version iri of the output file
+    #[arg(short = 'v', long = "version-iri", value_name = "iri")]
+    version_iri: Option<String>,
+}
+
+#[derive(Args, Debug)]
+struct SubsetArgs {
+    ///load ontology from a file
+    #[arg(short, long, value_name = "file")]
+    input: String,
+
+    ///save ontology to a file
+    #[arg(short, long, value_name = "file")]
+    output: String,
+
+    ///term to extract
+    #[arg(short = 't', long = "term", value_name = "term")]
+    term: Option<Vec<String>>,
+
+    ///load terms from a file
+    #[arg(short = 'T', long = "term-file", value_name = "textfile")]
+    term_file: Option<Vec<String>>,
 
     ///set the version iri of the output file
     #[arg(short = 'v', long = "version-iri", value_name = "iri")]
@@ -177,168 +213,68 @@ fn get_descendents(upper_terms: HashSet<String>, graph: &IndexedMemoryGraph) -> 
     output_terms
 }
 
-// Produce a MemoryGraph with desired terms and all necessary metadata
-fn mireot(
-    graph: &IndexedMemoryGraph,
-    terms: HashSet<String>,
-    version_iri: Option<String>,
-) -> MemoryGraph {
-    let mut output_graph = MemoryGraph::new();
-    let mut ontology = Subject::from_type("http://www.w3.org/2002/07/owl#Ontology");
-    match version_iri {
-        Some(iri) => {
-            ontology.insert("http://www.w3.org/2002/07/owl#versionIRI", Object::id(&iri));
-        }
-        None => (),
-    }
-    ontology.set_name("http://example.com/expected/mireot.owl");
-    output_graph.insert(ontology);
-
-    let mut metadata: HashSet<String> = vec![
-        "http://www.w3.org/2000/01/rdf-schema#comment",
-        "http://www.w3.org/2000/01/rdf-schema#label",
-        "http://www.w3.org/2000/01/rdf-schema#isDefinedBy",
-    ]
-    .iter()
-    .map(|x| x.to_string())
-    .collect();
-
-    // identify annotation properties used by desired terms
-    for subject in graph.subjects() {
-        // catch annotation properties used in relevant axiom annotations
-        let mut working_properties: HashSet<String> = HashSet::new();
-        if subject.owl_types().contains(AXIOM) {
-            let preds = subject.predicates();
-            if let Some(source) = preds.get(ANNOTATED_SOURCE) {
-                let mut source_in_terms = false;
-                for obj in source {
-                    if terms.contains(&obj.object()) {
-                        source_in_terms = true;
-                        break;
-                    }
-                }
-                if let Some(target) = preds.get(ANNOTATED_TARGET) {
-                    let mut target_in_terms = false;
-                    for obj in target {
-                        if terms.contains(&obj.object()) {
-                            target_in_terms = true;
-                            break;
-                        }
-                    }
-                    if source_in_terms && target_in_terms {
-                        for (pred, _objs) in preds {
-                            if pred != "http://www.geneontology.org/formats/oboInOwl#notes" {
-                                metadata.insert(pred.to_string());
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        // recursively identify metadata used in the terms themselves
-        if terms.contains(&subject.name()) {
-            for (pred, _objs) in subject.predicates() {
-                if let Some(pred_as_subj) = graph.get(&pred) {
-                    if pred_as_subj.owl_types().contains(ANNOTATION_PROPERTY) {
-                        working_properties.insert(pred);
-                        while working_properties.len() != 0 {
-                            let mut next_round: HashSet<String> = HashSet::new();
-                            for property in working_properties.clone() {
-                                metadata.insert(property.clone());
-                                if let Some(prop_as_subj) = graph.get(&property) {
-                                    if prop_as_subj.owl_types().contains(ANNOTATION_PROPERTY) {
-                                        for (pred, _objs) in prop_as_subj.predicates() {
-                                            if !metadata.contains(&pred) {
-                                                next_round.insert(pred);
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                            working_properties = next_round;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    //copy subjects in terms or metadata
-    for subject in graph.subjects() {
-        if subject.name() == "http://example.com/graph" {
-            output_graph.insert(subject.clone());
-        } else if terms.contains(&subject.name()) || metadata.contains(&subject.name()) {
-            let mut term = Subject::from_name(&subject.name());
-            for (pred, objs) in subject.predicates() {
-                for obj in objs {
-                    if pred == SUBCLASS_OF {
-                        if terms.contains(&obj.object())
-                            || obj.object() == "http://www.w3.org/2002/07/owl#Thing"
-                        {
-                            term.insert(&pred, obj.clone());
-                        }
-                    } else if pred == "http://www.w3.org/2002/07/owl#equivalentClass" {
-                        continue;
-                    } else if pred == "http://www.w3.org/2002/07/owl#disjointWith" {
-                        continue;
-                    } else if pred == "http://www.w3.org/2000/01/rdf-schema#subPropertyOf" {
-                        continue; // this is correct but it may not be intended behavior
-                    } else if pred == "http://www.w3.org/2000/01/rdf-schema#range"
-                        || obj.object() == "http://www.w3.org/2001/XMLSchema#anyURI"
-                    {
-                        continue;
-                    } else {
-                        term.insert(&pred, obj.clone());
-                    }
-                }
-            }
-            output_graph.insert(term);
-        }
-    }
-    output_graph
-}
-
 fn main() {
-    let args = Args::parse();
-
-    let input_path: String = args.input;
-    let input_path = Path::new(&input_path);
-    match fs::metadata(input_path) {
-        Ok(_) => (),
-        Err(_) => panic!("Input file does not exist"),
-    }
-
-    let branch_from = gather_terms_from_arg(args.branch_from_term, args.branch_from_terms);
-    let lower_terms = gather_terms_from_arg(args.lower_term, args.lower_terms);
-    let upper_terms = gather_terms_from_arg(args.upper_term, args.upper_terms);
-
-    let output_path: String = args.output;
-    let output_path = Path::new(&output_path);
-
-    let rdfxml_input = std::fs::read_to_string(input_path).expect("Read from file");
-    let graph = rdfxml::read(&rdfxml_input).expect("Read from string");
-    let graph = IndexedMemoryGraph::from(graph);
-
-    let output_terms: HashSet<String> = match branch_from {
-        Some(terms) => get_descendents(terms, &graph),
-        None => match lower_terms {
-            Some(lowers) => {
-                let ancestors = get_ancestors(lowers, &graph);
-                match upper_terms {
-                    Some(uppers) => {
-                        let descendents = get_descendents(uppers, &graph);
-                        filter_terms(ancestors, descendents)
-                    }
-                    None => ancestors,
-                }
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Mireot(mireot_args) => {
+            let input_path: String = mireot_args.input.clone();
+            let input_path = Path::new(&input_path);
+            match fs::metadata(input_path) {
+                Ok(_) => (),
+                Err(_) => panic!("Input file does not exist"),
             }
-            None => panic!(
-                "MISSING MIREOT TERMS ERROR either lower term(s) or branch term(s) must be specified for MIREOT\nFor details see: http://robot.obolibrary.org/extract#missing-mireot-terms-error"
-            ),
-        },
-    };
 
-    let output_graph = mireot(&graph, output_terms, args.version_iri);
-    let output = rdfxml::write_to_string(&output_graph).expect("Write to string");
-    std::fs::write(output_path, output).expect("Write to file");
+            let branch_from = gather_terms_from_arg(
+                mireot_args.branch_from_term.clone(),
+                mireot_args.branch_from_terms.clone(),
+            );
+            let lower_terms = gather_terms_from_arg(
+                mireot_args.lower_term.clone(),
+                mireot_args.lower_terms.clone(),
+            );
+            let upper_terms = gather_terms_from_arg(
+                mireot_args.upper_term.clone(),
+                mireot_args.upper_terms.clone(),
+            );
+
+            let output_path: String = mireot_args.output.clone();
+            let output_path = Path::new(&output_path);
+
+            let rdfxml_input = std::fs::read_to_string(input_path).expect("Read from file");
+            let graph = rdfxml::read(&rdfxml_input).expect("Read from string");
+            let graph = IndexedMemoryGraph::from(graph);
+            let output_terms: HashSet<String> = match branch_from {
+                Some(terms) => get_descendents(terms, &graph),
+                None => match lower_terms {
+                    Some(lowers) => {
+                        let ancestors = get_ancestors(lowers, &graph);
+                        match upper_terms {
+                            Some(uppers) => {
+                                let descendents = get_descendents(uppers, &graph);
+                                filter_terms(ancestors, descendents)
+                            }
+                            None => ancestors,
+                        }
+                    }
+                    None => panic!(
+                        "MISSING MIREOT TERMS ERROR either lower term(s) or branch term(s) must be specified for MIREOT\nFor details see: http://robot.obolibrary.org/extract#missing-mireot-terms-error"
+                    ),
+                },
+            };
+            let output_graph =
+                mireot_extract(&graph, output_terms, mireot_args.version_iri.clone());
+            let output = rdfxml::write_to_string(&output_graph).expect("Write to string");
+            std::fs::write(output_path, output).expect("Write to file");
+        }
+        Commands::Subset(subset_args) => {
+            let input_path: String = subset_args.input.clone();
+            let input_path = Path::new(&input_path);
+            match fs::metadata(input_path) {
+                Ok(_) => (),
+                Err(_) => panic!("Input file does not exist"),
+            }
+            // let terms =
+            //     gather_terms_from_arg(subset_args.term.clone(), subset_args.term_file.clone());
+        }
+    }
 }
---
+++ src/mireot.rs
@@ -0,0 +1,126 @@
+use crate::model::{
+    ANNOTATED_SOURCE, ANNOTATED_TARGET, ANNOTATION_PROPERTY, AXIOM, Graph, IndexedMemoryGraph,
+    MemoryGraph, Object, SUBCLASS_OF, Subject,
+};
+use std::collections::HashSet;
+
+// Produce a MemoryGraph with desired terms and all necessary metadata
+pub fn mireot_extract(
+    graph: &IndexedMemoryGraph,
+    terms: HashSet<String>,
+    version_iri: Option<String>,
+) -> MemoryGraph {
+    let mut output_graph = MemoryGraph::new();
+    let mut ontology = Subject::from_type("http://www.w3.org/2002/07/owl#Ontology");
+    match version_iri {
+        Some(iri) => {
+            ontology.insert("http://www.w3.org/2002/07/owl#versionIRI", Object::id(&iri));
+        }
+        None => (),
+    }
+    ontology.set_name("http://example.com/expected/mireot.owl");
+    output_graph.insert(ontology);
+
+    let mut metadata: HashSet<String> = vec![
+        "http://www.w3.org/2000/01/rdf-schema#comment",
+        "http://www.w3.org/2000/01/rdf-schema#label",
+        "http://www.w3.org/2000/01/rdf-schema#isDefinedBy",
+    ]
+    .iter()
+    .map(|x| x.to_string())
+    .collect();
+
+    // identify annotation properties used by desired terms
+    for subject in graph.subjects() {
+        // catch annotation properties used in relevant axiom annotations
+        let mut working_properties: HashSet<String> = HashSet::new();
+        if subject.owl_types().contains(AXIOM) {
+            let preds = subject.predicates();
+            if let Some(source) = preds.get(ANNOTATED_SOURCE) {
+                let mut source_in_terms = false;
+                for obj in source {
+                    if terms.contains(&obj.object()) {
+                        source_in_terms = true;
+                        break;
+                    }
+                }
+                if let Some(target) = preds.get(ANNOTATED_TARGET) {
+                    let mut target_in_terms = false;
+                    for obj in target {
+                        if terms.contains(&obj.object()) {
+                            target_in_terms = true;
+                            break;
+                        }
+                    }
+                    if source_in_terms && target_in_terms {
+                        for (pred, _objs) in preds {
+                            if pred != "http://www.geneontology.org/formats/oboInOwl#notes" {
+                                metadata.insert(pred.to_string());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // recursively identify metadata used in the terms themselves
+        if terms.contains(&subject.name()) {
+            for (pred, _objs) in subject.predicates() {
+                if let Some(pred_as_subj) = graph.get(&pred) {
+                    if pred_as_subj.owl_types().contains(ANNOTATION_PROPERTY) {
+                        working_properties.insert(pred);
+                        while working_properties.len() != 0 {
+                            let mut next_round: HashSet<String> = HashSet::new();
+                            for property in working_properties.clone() {
+                                metadata.insert(property.clone());
+                                if let Some(prop_as_subj) = graph.get(&property) {
+                                    if prop_as_subj.owl_types().contains(ANNOTATION_PROPERTY) {
+                                        for (pred, _objs) in prop_as_subj.predicates() {
+                                            if !metadata.contains(&pred) {
+                                                next_round.insert(pred);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            working_properties = next_round;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //copy subjects in terms or metadata
+    for subject in graph.subjects() {
+        if subject.name() == "http://example.com/graph" {
+            output_graph.insert(subject.clone());
+        } else if terms.contains(&subject.name()) || metadata.contains(&subject.name()) {
+            let mut term = Subject::from_name(&subject.name());
+            for (pred, objs) in subject.predicates() {
+                for obj in objs {
+                    if pred == SUBCLASS_OF {
+                        if terms.contains(&obj.object())
+                            || obj.object() == "http://www.w3.org/2002/07/owl#Thing"
+                        {
+                            term.insert(&pred, obj.clone());
+                        }
+                    } else if pred == "http://www.w3.org/2002/07/owl#equivalentClass" {
+                        continue;
+                    } else if pred == "http://www.w3.org/2002/07/owl#disjointWith" {
+                        continue;
+                    } else if pred == "http://www.w3.org/2000/01/rdf-schema#subPropertyOf" {
+                        continue; // this is correct but it may not be intended behavior
+                    } else if pred == "http://www.w3.org/2000/01/rdf-schema#range"
+                        || obj.object() == "http://www.w3.org/2001/XMLSchema#anyURI"
+                    {
+                        continue;
+                    } else {
+                        term.insert(&pred, obj.clone());
+                    }
+                }
+            }
+            output_graph.insert(term);
+        }
+    }
+    output_graph
+}
