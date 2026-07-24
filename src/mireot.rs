6a3059e2c3aa3c74999926487e8417244d993475
Sebastian Duesing <66700705+sebastianduesing@users.noreply.github.com>
2026-07-24 12:10:05 -0700

    Improve annotation property collection method

--- src/mireot.rs
+++ src/mireot.rs
@@ -1,8 +1,7 @@
 use crate::model::{
-    ANNOTATED_SOURCE, ANNOTATED_TARGET, ANNOTATION_PROPERTY, AXIOM, Graph, IndexedMemoryGraph,
-    MemoryGraph, Object, SUBCLASS_OF, Subject,
+    ANNOTATION_PROPERTY, Graph, IndexedMemoryGraph, MemoryGraph, Object, SUBCLASS_OF, Subject,
 };
-use std::collections::HashSet;
+use std::collections::{BTreeSet, HashSet};
 
 // Produce a MemoryGraph with desired terms and all necessary metadata
 pub fn mireot_extract(
@@ -21,70 +20,54 @@ pub fn mireot_extract(
     ontology.set_name("http://example.com/expected/mireot.owl");
     output_graph.insert(ontology);
 
-    let mut metadata: HashSet<String> = vec![
-        "http://www.w3.org/2000/01/rdf-schema#comment",
-        "http://www.w3.org/2000/01/rdf-schema#label",
-        "http://www.w3.org/2000/01/rdf-schema#isDefinedBy",
-    ]
-    .iter()
-    .map(|x| x.to_string())
-    .collect();
-
-    // identify annotation properties used by desired terms
-    for subject in graph.subjects() {
-        // catch annotation properties used in relevant axiom annotations
-        let mut working_properties: HashSet<String> = HashSet::new();
-        if subject.owl_types().contains(AXIOM) {
-            let preds = subject.predicates();
-            if let Some(source) = preds.get(ANNOTATED_SOURCE) {
-                let mut source_in_terms = false;
-                for obj in source {
-                    if terms.contains(&obj.object()) {
-                        source_in_terms = true;
-                        break;
-                    }
-                }
-                if let Some(target) = preds.get(ANNOTATED_TARGET) {
-                    let mut target_in_terms = false;
-                    for obj in target {
-                        if terms.contains(&obj.object()) {
-                            target_in_terms = true;
-                            break;
-                        }
-                    }
-                    if source_in_terms && target_in_terms {
-                        for (pred, _objs) in preds {
-                            if pred != "http://www.geneontology.org/formats/oboInOwl#notes" {
-                                metadata.insert(pred.to_string());
-                            }
-                        }
-                    }
-                }
-            }
+    // gather annotation properties used in the output
+    let mut annotation_props: HashSet<String> =
+        vec!["http://www.w3.org/2000/01/rdf-schema#isDefinedBy"]
+            .iter()
+            .map(|x| x.to_string())
+            .collect();
+    let mut predicates_to_check: BTreeSet<String> = graph
+        .subjects()
+        .iter()
+        .filter(|s| terms.contains(&s.name()))
+        .map(|s| s.predicates().keys().map(|x| x.to_string()).collect())
+        .collect::<BTreeSet<BTreeSet<String>>>()
+        .iter()
+        .flat_map(|s| s.iter())
+        .cloned()
+        .collect();
+    while predicates_to_check.len() > 0 {
+        let predicate = predicates_to_check
+            .pop_first()
+            .expect("at least one predicate");
+        if annotation_props.contains(&predicate) {
+            continue;
         }
-        // recursively identify metadata used in the terms themselves
-        if terms.contains(&subject.name()) {
-            for (pred, _objs) in subject.predicates() {
-                if let Some(pred_as_subj) = graph.get(&pred) {
-                    if pred_as_subj.owl_types().contains(ANNOTATION_PROPERTY) {
-                        working_properties.insert(pred);
-                        while working_properties.len() != 0 {
-                            let mut next_round: HashSet<String> = HashSet::new();
-                            for property in working_properties.clone() {
-                                metadata.insert(property.clone());
-                                if let Some(prop_as_subj) = graph.get(&property) {
-                                    if prop_as_subj.owl_types().contains(ANNOTATION_PROPERTY) {
-                                        for (pred, _objs) in prop_as_subj.predicates() {
-                                            if !metadata.contains(&pred) {
-                                                next_round.insert(pred);
-                                            }
-                                        }
-                                    }
-                                }
+        if let Some(pred_as_subj) = graph.get(&predicate) {
+            if pred_as_subj.owl_types().contains(ANNOTATION_PROPERTY) {
+                annotation_props.insert(predicate);
+                let mut working_properties: BTreeSet<String> = pred_as_subj
+                    .predicates()
+                    .keys()
+                    .map(|x| x.to_string())
+                    .filter(|x| !annotation_props.contains(x))
+                    .collect();
+                while working_properties.len() > 0 {
+                    let mut next_round: BTreeSet<String> = BTreeSet::new();
+                    for property in working_properties.clone() {
+                        annotation_props.insert(property.clone());
+                        if let Some(prop_as_subj) = graph.get(&property) {
+                            if prop_as_subj.owl_types().contains(ANNOTATION_PROPERTY) {
+                                next_round = prop_as_subj
+                                    .predicates()
+                                    .keys()
+                                    .map(|x| x.to_string())
+                                    .filter(|x| !annotation_props.contains(x))
+                                    .collect();
                             }
-                            working_properties = next_round;
                         }
                     }
+                    working_properties = next_round;
                 }
             }
         }
@@ -94,7 +77,7 @@ pub fn mireot_extract(
     for subject in graph.subjects() {
         if subject.name() == "http://example.com/graph" {
             output_graph.insert(subject.clone());
-        } else if terms.contains(&subject.name()) || metadata.contains(&subject.name()) {
+        } else if terms.contains(&subject.name()) || annotation_props.contains(&subject.name()) {
             let mut term = Subject::from_name(&subject.name());
             for (pred, objs) in subject.predicates() {
                 for obj in objs {
